165772320e44bf3f636c980c4ff9e8287dc5bf00
Piotr Gularski <piotr.gularski@gmail.com>
2016-05-26 17:09:42 +0100

    UTs added

--- test/test_smjs.js
+++ test/test_smjs.js
@@ -192,4 +192,158 @@
         assert.equal(m.leaf_state().name, 's211');
     });
 
+
+    QUnit.test( "Transitions with actions", function( assert ) {
+        var foo = true;
+
+        var test_list = [];
+        var clear_test_list = function () {
+            while(test_list.length > 0){
+                test_list.pop();
+            }
+        };
+
+        var on_enter = function(state, event) {
+            test_list.push(['enter', state.name]);
+        };
+        var on_exit = function(state, event){
+            test_list.push(['exit', state.name]);
+        };
+
+        var action_i = function (state, event) {
+            test_list.push('action_i');
+        };
+
+        var action_j = function (state, event) {
+            test_list.push('action_j');
+        };
+
+        var action_k = function (state, event) {
+            test_list.push('action_k');
+        };
+
+        var action_l = function (state, event) {
+            test_list.push('action_l');
+        };
+
+        var action_m = function (state, event) {
+            test_list.push('action_m');
+        };
+
+        var action_n = function (state, event) {
+            test_list.push('action_n');
+        };
+
+        var unset_foo = function (state, event) {
+            test_list.push('unset_foo');
+            foo = false;
+        };
+
+        var set_foo = function (state, event) {
+            test_list.push('set_foo');
+            foo = true;
+        };
+
+        var states = {};
+        var m = buildMachine(states);
+        var s0 = states.s0;
+        var s1 = states.s1;
+        var s11 = states.s11;
+        var s2 = states.s2;
+        var s21 = states.s21;
+        var s211 = states.s211;
+
+        // Internal transitions
+        m.add_transition(s0, null, events='i', null, action=action_i);
+        s0.add_transition(s1, null, events='j', null, action=action_j);
+        s0.add_transition(s2, null, events='k', null, action=action_k);
+        s1.add_transition(s11, null, events='n', null, action=action_n);
+        s1.add_transition(s11, null, events='h', null, action=unset_foo,
+            condition=function(s, e) {return foo === true });
+        s2.add_transition(s21, null, events='l', null, action=action_l,
+            condition=function(s, e) {return foo === true});
+        s21.add_transition(s211, null, events='m', null, action=action_m);
+        // External transition
+        s2.add_transition(s21, s21, events='h', null, action=set_foo,
+            condition=function(s, e) {return foo === false});
+
+        for (var name in states){
+            states[name].handlers = {'enter': on_enter, 'exit': on_exit}
+        }
+
+        m.initialize();
+
+        clear_test_list();
+        m.dispatch(new _e('i'));
+        assert.deepEqual(test_list, ['action_i']);
+        assert.equal(m.leaf_state().name, 's11');
+
+        clear_test_list();
+        m.dispatch(new _e('j'))
+        assert.deepEqual(test_list, ['action_j']);
+        assert.equal(m.leaf_state().name, 's11');
+
+        clear_test_list();
+        m.dispatch(new _e('n'))
+        assert.deepEqual(test_list, ['action_n']);
+        assert.equal(m.leaf_state().name, 's11');
+
+        // This transition toggles state between s11 and s211
+        m.dispatch(new _e('c'))
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        m.dispatch(new _e('i'))
+        assert.deepEqual(test_list, ['action_i']);
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        m.dispatch(new _e('k'))
+        assert.deepEqual(test_list, ['action_k']);
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        m.dispatch(new _e('m'))
+        assert.deepEqual(test_list, ['action_m']);
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        m.dispatch(new _e('n'))
+        assert.deepEqual(test_list, []);
+        assert.equal(m.leaf_state().name, 's211');
+
+        // This transition toggles state between s11 and s211
+        m.dispatch(new _e('c'))
+        assert.equal(m.leaf_state().name, 's11');
+
+        clear_test_list();
+        assert.equal(foo, true);
+        m.dispatch(new _e('h'))
+        assert.equal(foo, false);
+        assert.deepEqual(test_list, ['unset_foo']);
+        assert.equal(m.leaf_state().name, 's11');
+
+        clear_test_list();
+        m.dispatch(new _e('h'))
+        assert.deepEqual(test_list, []);  // Do nothing if foo is false
+        assert.equal(m.leaf_state().name, 's11');
+
+        // This transition toggles state between s11 and s211
+        m.dispatch(new _e('c'))
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        assert.equal(foo, false);
+        m.dispatch(new _e('h'));
+        assert.deepEqual(test_list, [['exit', 's211'], ['exit', 's21'], 'set_foo', ['enter', 's21'], ['enter', 's211']]);
+        assert.equal(foo, true);
+        assert.equal(m.leaf_state().name, 's211');
+
+        clear_test_list();
+        m.dispatch(new _e('h'))
+        assert.deepEqual(test_list, []);
+        assert.equal(m.leaf_state().name, 's211');
+
+    });
+
 }());
