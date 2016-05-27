b65bb15a71e007d47a34f7123592c793cd153a4a
Piotr Gularski <piotr.gularski@gmail.com>
2016-05-27 10:05:46 +0100

    Build options updated. Commented out code removed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,19 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                mangle: false
+
             },
             build: {
                 src: 'src/<%= pkg.shortName %>.js',
-                dest: 'build/<%= pkg.shortName %>.min.js'
+                dest: 'build/<%= pkg.shortName %>-<%= pkg.version %>.min.js'
+            },
+        },
+        copy: {
+            build: {
+                src: 'src/<%= pkg.shortName %>.js',
+                dest: 'build/<%= pkg.shortName %>-<%= pkg.version %>.js'
             }
         },
         qunit: {
@@ -38,10 +46,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['test', 'uglify']);
+    grunt.registerTask('build', ['test', 'uglify', 'copy']);
     grunt.registerTask('test', ['connect', 'jshint', 'qunit']);
 
 };
